d28bc628f492b22cba757d206a1c0ae9a0da4301
Andrew J Yao <yaoandrew@gmail.com>
2016-07-17 22:54:40 -0700

    [feature] adds code for stack

---
+++ stack/stack.js
@@ -0,0 +1,22 @@
+var Stack = function() {
+  this.count = 0;
+
+  };
+
+Stack.prototype.push = function(value){
+    this[this.count] = value;
+    this.count++;
+};
+Stack.prototype.pop = function(){
+    if(this.count > 0){
+      this.count--;
+      var temp = this[this.count];
+      delete this[this.count];
+      return temp;
+    }
+};
+Stack.prototype.size = function(){
+  return this.count;
+};
+
+var storage = new Stack();
